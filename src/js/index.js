3b188b1323597084bfe470d9603c21ccc7b1b6bd
Brian O'Toole <brian@sparxoo.com>
2018-03-31 11:08:13 -0400

    add nav

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,9 @@
 // extract text from webpack to bundled output file
 require("../scss/style.scss");
 
-//import MobileMenu from './MobileMenu';
-//import Modal from './Modal';
+import MobileMenu from './MobileMenu';
+var mobileMenu = new MobileMenu();
 
-//var mobileMenu = new MobileMenu();
+
+//import Modal from './Modal';
 //var modal = new Modal();
