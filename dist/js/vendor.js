2f044fbe7a2122fce246e419393d0e8f87b650f5
Brian O'Toole <brian@sparxoo.com>
2018-03-31 10:42:55 -0400

    add project layout

--- dist/js/vendor.js
+++ dist/js/vendor.js
@@ -60,22 +60,22 @@
 /******/ 	__webpack_require__.p = "../../";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 7);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
-/******/ ({
-
-/***/ 7:
+/******/ ([
+/* 0 */,
+/* 1 */,
+/* 2 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(8);
+__webpack_require__(3);
 
 /***/ }),
-
-/***/ 8:
+/* 3 */
 /***/ (function(module, exports) {
 
 (function(window, factory) {
@@ -779,5 +779,4 @@ __webpack_require__(8);
 
 
 /***/ })
-
-/******/ });
+/******/ ]);
