bd2cb297a4b89e39d3ed5fb0ff1629171884e514
Brian O'Toole <brian@sparxoo.com>
2018-04-12 15:38:47 -0400

    update fav

--- dist/js/app.js
+++ dist/js/app.js
@@ -60,97 +60,11 @@
 /******/ 	__webpack_require__.p = "../../";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-var _MobileMenu = __webpack_require__(4);
-
-var _MobileMenu2 = _interopRequireDefault(_MobileMenu);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-/**
-  * MAIN APP SCRIPTS ENTRY POINT
-  */
-
-// extract text from webpack to bundled output file
-__webpack_require__(1);
-__webpack_require__(6);
-__webpack_require__(7);
-
-var mobileMenu = new _MobileMenu2.default();
-
-/***/ }),
-/* 1 */
-/***/ (function(module, exports) {
-
-// removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 2 */,
-/* 3 */,
-/* 4 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /**
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       * MOBILE MENU
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       */
-
-var _jquery = __webpack_require__(5);
-
-var _jquery2 = _interopRequireDefault(_jquery);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var MobileMenu = function () {
-  function MobileMenu() {
-    _classCallCheck(this, MobileMenu);
-
-    this.body = (0, _jquery2.default)('body');
-    this.siteHeader = (0, _jquery2.default)('.header');
-    this.menuIcon = (0, _jquery2.default)('.header__menu-icon');
-    this.menuContent = (0, _jquery2.default)('.header__menu-content');
-    this.events();
-  }
-
-  _createClass(MobileMenu, [{
-    key: 'events',
-    value: function events() {
-      this.menuIcon.click(this.toggleMenu.bind(this));
-    }
-  }, {
-    key: 'toggleMenu',
-    value: function toggleMenu() {
-      this.body.toggleClass('menu-is-open');
-      this.menuContent.toggleClass('header__menu-content--is-open');
-      this.siteHeader.toggleClass('header--is-open');
-      this.menuIcon.toggleClass('header__menu-icon--close');
-    }
-  }]);
-
-  return MobileMenu;
-}();
-
-exports.default = MobileMenu;
-
-/***/ }),
-/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;/*!
@@ -10521,13 +10435,97 @@ return jQuery;
 
 
 /***/ }),
-/* 6 */
+/* 1 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _jquery = __webpack_require__(5);
+var _MobileMenu = __webpack_require__(2);
+
+var _MobileMenu2 = _interopRequireDefault(_MobileMenu);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+  * MAIN APP SCRIPTS ENTRY POINT
+  */
+
+// extract text from webpack to bundled output file
+__webpack_require__(3);
+__webpack_require__(4);
+__webpack_require__(5);
+
+var mobileMenu = new _MobileMenu2.default();
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /**
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       * MOBILE MENU
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       */
+
+var _jquery = __webpack_require__(0);
+
+var _jquery2 = _interopRequireDefault(_jquery);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var MobileMenu = function () {
+  function MobileMenu() {
+    _classCallCheck(this, MobileMenu);
+
+    this.body = (0, _jquery2.default)('body');
+    this.siteHeader = (0, _jquery2.default)('.header');
+    this.menuIcon = (0, _jquery2.default)('.header__menu-icon');
+    this.menuContent = (0, _jquery2.default)('.header__menu-content');
+    this.events();
+  }
+
+  _createClass(MobileMenu, [{
+    key: 'events',
+    value: function events() {
+      this.menuIcon.click(this.toggleMenu.bind(this));
+    }
+  }, {
+    key: 'toggleMenu',
+    value: function toggleMenu() {
+      this.body.toggleClass('menu-is-open');
+      this.menuContent.toggleClass('header__menu-content--is-open');
+      this.siteHeader.toggleClass('header--is-open');
+      this.menuIcon.toggleClass('header__menu-icon--close');
+    }
+  }]);
+
+  return MobileMenu;
+}();
+
+exports.default = MobileMenu;
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _jquery = __webpack_require__(0);
 
 var _jquery2 = _interopRequireDefault(_jquery);
 
@@ -10554,13 +10552,13 @@ function rotateTerm2() {
 (0, _jquery2.default)(rotateTerm2);
 
 /***/ }),
-/* 7 */
+/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-var _jquery = __webpack_require__(5);
+var _jquery = __webpack_require__(0);
 
 var _jquery2 = _interopRequireDefault(_jquery);
 
--- dist/js/vendor.js
+++ dist/js/vendor.js
@@ -60,22 +60,22 @@
 /******/ 	__webpack_require__.p = "../../";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 2);
+/******/ 	return __webpack_require__(__webpack_require__.s = 6);
 /******/ })
 /************************************************************************/
-/******/ ([
-/* 0 */,
-/* 1 */,
-/* 2 */
+/******/ ({
+
+/***/ 6:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(3);
+__webpack_require__(7);
 
 /***/ }),
-/* 3 */
+
+/***/ 7:
 /***/ (function(module, exports) {
 
 (function(window, factory) {
@@ -779,4 +779,5 @@ __webpack_require__(3);
 
 
 /***/ })
-/******/ ]);
+
+/******/ });
