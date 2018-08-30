3c22e15b04f190a776744a12fde3fbc4d0e10a31
botoole <botoole518@gmail.com>
2018-08-30 14:30:01 -0400

    form

--- src/js/form.js
+++ src/js/form.js
@@ -33,7 +33,7 @@ function validateForm() {
 	  var datastring = $("#js-form").serialize();
 	  $.ajax({
 	    //send the form using formspree
-	    url: "https://formspree.io/botoole518@gmail.com", 
+	    url: "//formspree.io/brian@brickandmonitor.com", 
 	    method: "POST",
 	    data: datastring,
 	    dataType: "json"
