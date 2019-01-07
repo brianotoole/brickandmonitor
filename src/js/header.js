67c2cb3a6e52b1d66ba3e43a4faa5d2953ed5bdb
brianotoole <botoole518@gmail.com>
2019-01-07 16:08:01 -0500

    navtoggle

--- src/js/header.js
+++ src/js/header.js
@@ -5,10 +5,13 @@
 // Mobile nav
 var trigger = $('.js-hamburger');
 trigger.on('click', function() {
-  $('body').toggleClass('hamburger-menu-open, nav-open');
-	$(this).toggleClass('animate');
+  $('body').toggleClass('hamburger-menu-open nav-open');
+  $('.header__nav, .header__logo').removeClass('header--shrink-width');
+  $(this).toggleClass('animate');
+  //if ($(".nav-open").length > 0) {
+  $('.hamburger').addClass('hide');
+  //}   
 });
-
 
 // Hide Header on on scroll down
 var didScroll;
@@ -39,11 +42,13 @@ function hasScrolled() {
         // Scroll Down
         $('.header__nav, .header__logo').addClass('header--shrink-width');  
         $('.header').addClass('header--is-small'); 
+        $('.hamburger').removeClass('hide animate');
     } else {
         // Scroll Up
         if(st + $(window).height() < $(document).height()) {
           $('.header__nav, .header__logo').removeClass('header--shrink-width'); 
           $('.header').removeClass('header--is-small'); 
+          $('.hamburger').removeClass('hide animate');
         }
     }
     
