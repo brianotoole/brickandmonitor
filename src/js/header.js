420497a8c68d1c32ca50cb4115c91a0761f1f5b8
brianotoole <botoole518@gmail.com>
2019-01-06 20:40:27 -0500

    rm headroomjs

--- src/js/header.js
+++ src/js/header.js
@@ -2,32 +2,12 @@
   Nav
 *********************************************************/
 
-// Headroom.js
-// https://wicky.nillia.ms/headroom.js
-import Headroom from 'headroom.js/dist/headroom.js';
-window.Headroom = Headroom;
-import 'headroom.js/dist/jQuery.headroom.js';
-
-
-  var header_height = $('.header').outerHeight();
-  var logo_height = $('.hero__logo').outerHeight();
-  var offset_top = (logo_height - header_height);
-
-  //console.log(logo_height);
-
-  // Headroom.js scroll 
-  var header = new Headroom(document.querySelector(".header"), {
-    tolerance: 0,
-    offset : offset_top, //vertical offset in px before element is first unpinned
-    classes: {
-      initial: "header--fixed",
-      pinned: "slideDown",
-      unpinned: "slideUp",
-      top: "top",
-      notTop: "not-top"
-    }
-  });
-  header.init();
+// Mobile nav
+var trigger = $('.js-hamburger');
+trigger.on('click', function() {
+  $('body').toggleClass('hamburger-menu-open');
+	$(this).toggleClass('animate');
+});
 
 
 // Hide Header on on scroll down
@@ -69,13 +49,3 @@ function hasScrolled() {
     
     lastScrollTop = st;
 }
-
-
-
-
-// Mobile nav
-var trigger = $('.js-hamburger');
-trigger.on('click', function() {
-  $('body').toggleClass('hamburger-menu-open');
-	$(this).toggleClass('animate');
-});
