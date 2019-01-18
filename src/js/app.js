d56731ceb70fce42111dea8a7a1a9c6ed4d7ecd2
brianotoole <botoole518@gmail.com>
2019-01-17 23:13:33 -0500

    rm unused fullpagejs

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@ import 'styles/app.scss';
 //import $ from 'jquery';
 
 // Scripts
-require ('./fullpage.js');
+//require ('./fullpage.js');
 require ('./header.js');
 require ('./form.js');
 require ('./tweens.js');
